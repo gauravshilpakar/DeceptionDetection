8354c9cecb5b84a29ee09230c879a7498a378448
gauravshilpakar <gaurav.shilpakar@gmail.com>
2021-03-23 11:45:41 -0500

    paths and filename fix

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import os
 from pydub import AudioSegment
 from pytube import YouTube
 from pytube.cli import on_progress
+import shutil
 
 
 def replaceCharacters(str):
@@ -17,7 +18,7 @@ def replaceCharacters(str):
     return str
 
 
-def download(link):
+def download(link, downloadPath):
     '''
     Downloads YouTube video from the provided link.
     Saves to a folder named as the video title.
@@ -39,12 +40,12 @@ def download(link):
 
     yt.register_on_complete_callback(changeCompleteStatus(downloadStatus))
     yt.streams.filter(only_audio=True)[0].download(
-        trackTitle, filename=trackTitle)
+        f"{downloadPath}\\{trackTitle}", filename=trackTitle)
     print("\n---Download Complete!---")
     return trackTitle
 
 
-def clipAudio(audioFile, timeStamp, trackTitle):
+def clipAudio(audioFile, timeStamp, trackTitle, downloadPath):
     '''
     Cuts an audio clip as per the time stamps provided.
 
@@ -66,12 +67,12 @@ def clipAudio(audioFile, timeStamp, trackTitle):
 
         startTime = (int(startMin) * 60 + int(startSec)) * 1000
         finishTime = (int(finishMin) * 60 + int(finishSec)) * 1000
-        try:
-            cutAudio = audioFile[startTime:finishTime]
-            cutAudio.export(
-                f"{os.getcwd()}/{trackTitle}/{exportName}{count}.mp3", format="mp3")
-        except:
-            print("Couldn't export clip of time stamp: {start}-{finish}")
+        # try:
+        cutAudio = audioFile[startTime:finishTime]
+        cutAudio.export(
+            f"{downloadPath}/{trackTitle}/{exportName}{count}.mp3", format="mp3")
+        # except:
+        #     print("Couldn't export clip of time stamp: {start}-{finish}")
 
         count = count + 1
     print("\n---Clipping Complete---")
@@ -83,13 +84,14 @@ if __name__ == "__main__":
     link = lines[0]
 
     print("YouTube link received: " + link, end='')
+    downloadPath = os.path.join(os.getcwd(), "downloads")
 
-    trackTitle = download(link)
+    trackTitle = download(link, downloadPath)
 
     reqPath = os.path.join(
-        os.getcwd(), trackTitle, trackTitle.replace(',', '') + ".mp4")
+        os.getcwd(), "downloads", trackTitle, trackTitle.replace(',', '') + ".mp4")
     audioFile = AudioSegment.from_file(reqPath, "mp4")
 
-    count = 0
     exportName = trackTitle.split(' ')[0]
-    clipAudio(audioFile, lines, trackTitle)
+    clipAudio(audioFile, lines, trackTitle, downloadPath)
+    shutil.copy2('Link.txt', os.path.join(downloadPath, trackTitle))
